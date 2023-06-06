9fb1907753e4dbe83915b5448e5f24bb3b1a1c66
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-06-06 20:50:58 +0200

    s

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,6 @@ dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.8.21-1.0.11")
     implementation("org.json:json:20230227")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
