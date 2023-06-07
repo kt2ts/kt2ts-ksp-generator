119c7489dee497ad4caeb0112ae94e142510c1ad
mlo <mlo@mlos-Mac-mini.local>
2023-06-07 18:11:23 +0200

    SAVEPOINT

--- src/main/kotlin/kt2ts/kspgenerator/utils/ShellRunner.kt
+++ src/main/kotlin/kt2ts/kspgenerator/utils/ShellRunner.kt
@@ -6,18 +6,10 @@ import java.util.Scanner
 
 object ShellRunner {
 
-    data class CommandResult(
-        val result: Int,
-        val output: List<String>,
-        val errorOutput: List<String>
-    )
-
-    fun run(command: String, vararg params: String): CommandResult = doRun(null, command, *params)
-
-    fun run(directory: Path, command: String, vararg params: String): CommandResult =
+    fun run(directory: Path, command: String, vararg params: String) =
         doRun(directory, command, *params)
 
-    private fun doRun(directory: Path?, command: String, vararg params: String): CommandResult {
+    private fun doRun(directory: Path?, command: String, vararg params: String) {
         val builder =
             ProcessBuilder().apply {
                 environment().apply {
@@ -30,13 +22,7 @@ object ShellRunner {
             }
         val fullCommand = command + params.fold("") { acc, s -> "$acc $s" }
         builder.command("sh", "-c", fullCommand)
-        val process = builder.start()
-        val (output, outputThread) = outputThread(process.inputStream, "Command output:")
-        val (error, errorThread) = outputThread(process.errorStream, "Command error:")
-        val result = process.waitFor()
-        outputThread.join()
-        errorThread.join()
-        return CommandResult(result, output, error)
+        builder.start()
     }
 
     fun outputThread(inputStream: InputStream, logPrefix: String): Pair<List<String>, Thread> {
