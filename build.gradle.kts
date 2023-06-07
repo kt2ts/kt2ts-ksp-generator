16c9ecfdd5d35c3adaeef1125e763aea6aa231f4
mlo <mlo@mlos-Mac-mini.local>
2023-06-07 19:00:14 +0200

    SAVEPOINT

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "io.github.kt2ts"
             artifactId = "kt2ts-ksp-generator"
-            version = "0.0.2"
+            version = "0.0.1"
 
             from(components["kotlin"])
         }
